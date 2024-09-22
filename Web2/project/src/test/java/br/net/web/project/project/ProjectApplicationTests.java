412eedcf66dc9f41a0574e62a144bf754189f50f
Gabriel Yan Russo <49681781+TrivialX@users.noreply.github.com>
2024-09-21 22:44:01 -0300

    InitProject

---
+++ Web2/project/src/test/java/br/net/web/project/project/ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.net.web.project.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
